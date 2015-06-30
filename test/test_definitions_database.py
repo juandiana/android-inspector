12b398d1cb738c090743750f845a9ca0b4d1b4f4
Juan Andrés Diana <juandiana@gmail.com>
2015-06-30 02:00:40 -0300

    - Split the SQL script in two : create_db.sql & insert_email_operation.sql. - Started the DefinitionsDatabase implementation. -

---
+++ test/test_definitions_database.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+import unittest
+from components.definitions_database import DefinitionsDatabase
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        db_helper = DefinitionsDatabase()
+
+
+if __name__ == '__main__':
+    unittest.main()
