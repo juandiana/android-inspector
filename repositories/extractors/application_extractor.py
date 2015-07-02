cdb96bd65e53538d727c7cd7bf40df1fcefad087
Juan Andrés Diana <juandiana@gmail.com>
2015-07-01 21:11:36 -0300

    Fixes ApplicationExtractor

--- repositories/extractors/application_extractor.py
+++ repositories/extractors/application_extractor.py
@@ -5,16 +5,15 @@ from model.operation import Extractor, OperationError
 
 
 class ApplicationExtractor(Extractor):
-    def execute(self, route, param_values):
+    def execute(self, extracted_data_dir_path, param_values):
         command_name = "adb"
         command_arg1 = "pull"
         command_arg2 = "/data/data/{0}".format(param_values['package_name'])
 
         try:
-            p = subprocess.Popen(command_name + " " + command_arg1 + " " + command_arg2, shell=True, cwd=route,
-                                 stdin=subprocess.PIPE, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-            for line in p.stdout:
-                print "[adb] {0}".format(line.rstrip())
-                p.stdout.flush()
+            p = subprocess.Popen([command_name, command_arg1, command_arg2],
+                                 cwd=extracted_data_dir_path, stderr=subprocess.PIPE)
+            for line in p.stderr:
+                print "[adb] {0}".format(line)
         except subprocess.CalledProcessError:
             raise OperationError('Extraction failed.')
