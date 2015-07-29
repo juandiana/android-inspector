6a8cc01c2fb8dfe731a599c4f2598c35a7e60709
José Ignacio Varela <jivarela26@gmail.com>
2015-07-29 18:00:03 -0300

    Refactor tar extraction.

--- repositories/extractors/adb_backup_extractor.py
+++ repositories/extractors/adb_backup_extractor.py
@@ -1,64 +1,67 @@
 # coding=utf-8
-import glob
 import os
 import shutil
 import subprocess
+import tarfile
 from model import Extractor, OperationError
 
 
 class AdbBackupExtractor(Extractor):
     @staticmethod
-    def __adb_backup__(file_path, output_dir):
-        cmd = '/home/nacho/Apps/adt-bundle-linux-x86_64-20131030/sdk/platform-tools/adb backup -apk ' + file_path
-        p = subprocess.Popen(cmd.split(), cwd=output_dir, stderr=subprocess.PIPE)
+    def __adb_backup__(package_name):
+        cmd = 'adb backup -apk ' + package_name
+        p = subprocess.Popen(cmd.split(), stderr=subprocess.PIPE)
         for line in p.stderr:
             print '\t[adb] {0}'.format(line),
 
     @staticmethod
-    def __unpack__(jar_path, output_dir):
+    def __unpack__(jar_path):
         cmd = 'java -jar ' + os.path.join(jar_path, 'abe.jar') + ' unpack backup.ab backup.tar'
-        p = subprocess.Popen(cmd.split(), cwd=output_dir, stderr=subprocess.PIPE)
+        p = subprocess.Popen(cmd.split(), stderr=subprocess.PIPE)
         for line in p.stderr:
             print '\t[unpacking] {0}'.format(line),
 
     @staticmethod
-    def __extract_tar__(output_dir):
-        cmd = 'tar -xvf backup.tar'
-        p = subprocess.Popen(cmd.split(), cwd=output_dir, stderr=subprocess.PIPE)
-        for line in p.stderr:
-            print '\t[unpacking] {0}'.format(line),
+    def __extract_tar__():
+        tar_path = os.path.join("backup.tar")
+        tar = tarfile.open(tar_path)
+        tar.extractall()
+        tar.close()
 
     @staticmethod
-    def __order_data__(package_name, output_dir):
+    def __order_data__(package_name):
         # Copy extracted files to root output_dir
-        for extracted_file in os.listdir(os.path.join(output_dir, 'apps', package_name)):
-            current = os.path.join(output_dir, 'apps', package_name, extracted_file)
-            destination = os.path.join(output_dir, extracted_file)
+        for extracted_file in os.listdir(os.path.join('apps', package_name)):
+            current = os.path.join('apps', package_name, extracted_file)
+            destination = os.path.join(extracted_file)
             if os.path.isdir(current):
                 shutil.copytree(current, destination)
             else:
                 shutil.copy2(current, destination)
 
         # Delete apps directory and backups files
-        shutil.rmtree(os.path.join(output_dir, 'apps'))
-        os.remove(os.path.join(output_dir, 'backup.tar'))
-        os.remove(os.path.join(output_dir, 'backup.ab'))
+        shutil.rmtree('apps')
+        os.remove('backup.tar')
+        os.remove('backup.ab')
 
         # Rename extracted databases folder.
-        os.rename(os.path.join(output_dir, 'db'), os.path.join(output_dir, 'databases'))
+        os.rename('db', 'databases')
 
     def execute(self, extracted_data_dir_path, param_values):
         app_package_name = param_values['package_name']
+        jar_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
 
+        owd = os.getcwd()
+        os.chdir(extracted_data_dir_path)
         try:
-            self.__adb_backup__(app_package_name, extracted_data_dir_path)
+            self.__adb_backup__(app_package_name)
 
-            jar_path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-            self.__unpack__(jar_path, extracted_data_dir_path)
+            self.__unpack__(jar_path)
 
-            self.__extract_tar__(extracted_data_dir_path)
+            self.__extract_tar__()
 
-            self.__order_data__(app_package_name, extracted_data_dir_path)
+            self.__order_data__(app_package_name)
 
+            os.chdir(owd)
         except subprocess.CalledProcessError:
             raise OperationError('Extraction failed.')
